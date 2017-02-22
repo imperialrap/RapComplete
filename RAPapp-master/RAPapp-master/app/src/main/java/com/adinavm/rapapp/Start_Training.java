c5838074f577b66a619b42ea4e4b792fe2dd69b7
imperialrap <rapraprap1001@gmail.com>
2017-02-22 14:09:48 +0000

    with bluetooth changes

--- RAPapp-master/RAPapp-master/app/src/main/java/com/adinavm/rapapp/Start_Training.java
+++ RAPapp-master/RAPapp-master/app/src/main/java/com/adinavm/rapapp/Start_Training.java
@@ -1,108 +1,117 @@
 package com.adinavm.rapapp;
 
+import android.content.BroadcastReceiver;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.bluetooth.BluetoothAdapter;
 import android.widget.Button;
-import android.view.View.OnClickListener;
-import android.graphics.Point;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.view.LayoutInflater;
 import android.content.Context;
-import android.view.Gravity;
-import android.widget.PopupWindow;
-import android.graphics.drawable.BitmapDrawable;
 
 
 
 public class Start_Training extends AppCompatActivity {
 
-    //The "x" and "y" position of the "Show Button" on screen.
-    Point p;
+    private final String TAG = "Start_Training";
+
+    BluetoothAdapter mBluetoothAdapter;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start__training);
+        //initialise the button
+        Button button13 = (Button) findViewById(R.id.button13);
 
-        Button btn_show = (Button) findViewById(R.id.button13);
-        btn_show.setOnClickListener(new OnClickListener() {
+        Button button25 = (Button) findViewById(R.id.button25);
+
+        mBluetoothAdapter = mBluetoothAdapter.getDefaultAdapter();
+
+        button13.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View arg0) {
-                BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-                if (mBluetoothAdapter == null) {
-                    showPopup(Start_Training.this, p);
-                    // Device does not support Bluetooth - check if supported
-                } else {
-                    if (mBluetoothAdapter.isEnabled()) {
-                        showPopup(Start_Training.this, p);
-                        // Bluetooth not enabled
-                    }
-                }}});
+            public void onClickStartRecording(View view) {
+                enableDisableBT1();
+            }
+        });
+
+        button25.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClickStopRecording(View view) {
+                enableDisableBT2();
+            }
+        });
+
     }
+
+    public void enableDisableBT1(){
+        if(mBluetoothAdapter == null){
+            Log.d(TAG, "enableDisableBT: Does not have bluetooth capabilities");
+        }
+        else if(!mBluetoothAdapter.isEnabled()){
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivity(enableIntent);
+
+            IntentFilter BTIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+            registerReceiver(mBroadastReceiver1, BTIntent);
+        }
+        else if(mBluetoothAdapter.isEnabled()){
+            Log.d(TAG, "enableDisableBT: Bluetooth already ON");
+        }
+
+    }
+
+    public void enableDisableBT2(){
+        if(mBluetoothAdapter == null){
+            Log.d(TAG, "enableDisableBT: Does not have bluetooth capabilities");
+        }
+        else if(!mBluetoothAdapter.isEnabled()){
+            Log.d(TAG, "enableDisableBT: Bluetooth is not needed");
+        }
+        else if(mBluetoothAdapter.isEnabled()){
+            mBluetoothAdapter.disable();
+        }
+
+    }
+
+    private final BroadcastReceiver mBroadastReceiver1 = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = Intent.getAction();
+            if (action.equals(mBluetoothAdapter.ACTION_STATE_CHANGED)){
+                final int state = Intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, mBluetoothAdapter.ERROR);
+
+                switch(state){
+                    case BluetoothAdapter.STATE_OFF:
+                        Log.d(TAG, "Bluetooth: STATE OFF");
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_OFF:
+                        Log.d(TAG, "Bluetooth: STATE TURNING OFF");
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_ON:
+                        Log.d(TAG, "Bluetooth: STATE TURNING ON");
+                        break;
+                    case BluetoothAdapter.STATE_ON:
+                        Log.d(TAG, "Bluetooth: STATE ON");
+                        break;
+                }
+            }
+
+        }
+    };
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        unregisterReceiver(mBroadastReceiver1);
+    }
+
+
 
     // when you click the back button, it returns to Home_Screen
     public void onClickBack(View view) {
         Intent intent = new Intent(this, Home_Screen.class);
         startActivity(intent);
-    }
-
-    // Get the x and y position after the button is draw on screen
-    // (It's important to note that we can't get the position in the onCreate(),
-    // because at that stage most probably the view isn't drawn yet, so it will return (0, 0))
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-
-        int[] location = new int[2];
-        Button button = (Button) findViewById(R.id.button13);
-
-        // Get the x, y location and store it in the location[] array
-        // location[0] = x, location[1] = y.
-        button.getLocationOnScreen(location);
-
-        //Initialize the Point with x, and y positions
-        p = new Point();
-        p.x = location[0];
-        p.y = location[1];
-    }
-
-    // The method that displays the popup.
-    private void showPopup(final AppCompatActivity context, Point p) {
-        int popupWidth = 1000;
-        int popupHeight = 5000;
-
-        // Inflate the popup_layout.xml
-        LinearLayout viewGroup = (LinearLayout) context.findViewById(R.id.popup);
-        LayoutInflater layoutInflater = (LayoutInflater) context
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View layout = layoutInflater.inflate(R.layout.popup_layout, viewGroup);
-
-        // Creating the PopupWindow
-        final PopupWindow popup = new PopupWindow(context);
-        popup.setContentView(layout);
-        popup.setWidth(popupWidth);
-        popup.setHeight(popupHeight);
-        popup.setFocusable(true);
-
-        // Some offset to align the popup a bit to the right, and a bit down, relative to button's position.
-        int OFFSET_X = 200;
-        int OFFSET_Y = -2000;
-
-
-        // Displaying the popup at the specified location, + offsets.
-        popup.showAtLocation(layout, Gravity.NO_GRAVITY, p.x + OFFSET_X, p.y + OFFSET_Y);
-
-        // Getting a reference to Close button, and close the popup when clicked.
-        Button close = (Button) layout.findViewById(R.id.close);
-        close.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                popup.dismiss();
-            }
-        });
     }
 }
